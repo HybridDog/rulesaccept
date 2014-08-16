fff8a96537b098da9c9f19d8b9b0c88658c98765
HybridDog <adresse_test@t-online.de>
2014-08-16 23:03:03 +0200

    automatic update

--- init.lua
+++ init.lua
@@ -1,6 +1,4 @@
-local rulesaccept = {}
-
---http://minetest.web1337.net/rules.php
+--http://minetest.web1337.net/rules.php <rules of zegaton
 local rules = {
 	"Do not grief!",
 	"Do not flood the map with water or lava!",
@@ -13,76 +11,83 @@ local rules = {
 	"Respect other users and the mods/admins!",
 }
 
-local cnt = #rules
-rulesaccept.users = {}
-
-local font_size
-if minetest.is_singleplayer() then
-	font_size = tonumber(minetest.setting_get("font_size")) or 13
-else
-	font_size = 13
-end
-local corners = {x=0.1, y=-0.2}
-local fx = 15*font_size
-local fy = font_size/65
-
-local function text_to_labels(text, x, y)
-	local tab = minetest.splittext(text, fx)
-	local l = 0
-	text = ""
-	for _,str in ipairs(tab) do
-		text = text.."label["..0 ..","..y..";"..str.."]"
-		y = y+fy
-		l = math.max(l, #str)
+local cnt, corners, fx, fy, text_to_labels, get_formspec, rules_form, plist, waiters, font_size, stuff_defined
+local function define_stuff()
+	if minetest.is_singleplayer() then
+		font_size = tonumber(minetest.setting_get("font_size")) or 13
+	else
+		font_size = 13
 	end
-	x = math.max(x, l/font_size)
-	return text, x, y
-end
 
-local function get_formspec(num, cont)
-	local rule = rules[num]
+	cnt = #rules
+	corners = {x=0.1, y=-0.2}
+	fx = 15*font_size
+	fy = font_size/65
+
+	function text_to_labels(text, x, y)
+		local tab = minetest.splittext(text, fx)
+		local l = 0
+		text = ""
+		for _,str in ipairs(tab) do
+			text = text.."label["..0 ..","..y..";"..str.."]"
+			y = y+fy
+			l = math.max(l, #str)
+		end
+		x = math.max(x, l/font_size)
+		return text, x, y
+	end
+
+	function get_formspec(num, cont)
+		local rule = rules[num]
+		local y = corners.y
+		local x = corners.x
+
+		local text, x, y = text_to_labels(rule, x, y)
+		x = x+corners.x
+		x = math.max(x, 2)
+		y = y+1
+
+		local spec = "size["..x..","..y.."]"..
+			text..
+			"button_exit[0,"..y-0.5 ..";1,1;exit;exit]"
+		if cont then
+			spec = spec.."button["..x-1 ..","..y-0.5 ..";1,1;accept;accept]"
+		end
+		return spec
+	end
+
+	rules_form = ""
+	for _,rule in ipairs(rules) do
+		rules_form = rules_form..rule.."\n\n"
+	end
+	rules_form = string.sub(rules_form, 1, -3)
 	local y = corners.y
 	local x = corners.x
 
-	local text, x, y = text_to_labels(rule, x, y)
+	local text, x, y = text_to_labels(rules_form, x, y)
 	x = x+corners.x
 	x = math.max(x, 2)
 	y = y+1
 
-	local spec = "size["..x..","..y.."]"..
+	rules_form = "size["..x..","..y.."]"..
 		text..
-		"button_exit[0,"..y-0.5 ..";1,1;exit;exit]"
-	if cont then
-		spec = spec.."button["..x-1 ..","..y-0.5 ..";1,1;accept;accept]"
-	end
-	return spec
+		"button_exit["..x/2-1 ..","..y-0.5 ..";2,1;exit;exit]"
+
+	plist = {}
+	waiters = {}
 end
-
-local rules_form = ""
-for _,rule in ipairs(rules) do
-	rules_form = rules_form..rule.."\n\n"
-end
-rules_form = string.sub(rules_form, 1, -3)
-local y = corners.y
-local x = corners.x
-
-local text, x, y = text_to_labels(rules_form, x, y)
-x = x+corners.x
-x = math.max(x, 2)
-y = y+1
-
-rules_form = "size["..x..","..y.."]"..
-	text..
-	"button_exit["..x/2-1 ..","..y-0.5 ..";2,1;exit;exit]"
-
-local plist = {}
-local waiters = {}
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname ~= "rule"
 	or not player then
 		return
 	end
+
+	if not stuff_defined then
+		define_stuff()
+		stuff_defined = true
+	end
+
 	local pname = player:get_player_name()
 	if fields.accept then
 		local num = plist[pname] or 2
@@ -125,6 +130,12 @@ minetest.register_chatcommand("show_rules", {
 		if not privs.shout then
 			return true, "This command requires the shout privilege."
 		end
+
+		if not stuff_defined then
+			define_stuff()
+			stuff_defined = true
+		end
+
 		if privs.interact then
 			minetest.show_formspec(name, "rule", rules_form)
 			return true, "Showing rules..."
